6066aa7f54b6631a01ced69807c9bab3922361fd
bain tsyrempilov <batsbad@mail.ru>
2024-03-15 22:55:55 +0300

    some libs added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,9 +33,27 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
+    // retrofit
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+
+    implementation("org.kodein.di:kodein-di-generic-jvm:6.2.1")
+    implementation("org.kodein.di:kodein-di-framework-android-x:6.2.1")
+
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.0-alpha04")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.0-alpha04")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
